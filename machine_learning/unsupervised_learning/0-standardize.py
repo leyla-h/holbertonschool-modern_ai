4df82db503bd7d3b45f08a8f7ed66d1f77722a73
leyla-h <13678@holbertonstudents.com>
2026-05-26 14:50:23 +0400

    Create 0-standardize.py

---
+++ machine_learning/unsupervised_learning/0-standardize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module to standardize tabular data using Scikit-learn.
+"""
+from sklearn import preprocessing
+import numpy as np
+
+
+def Standardize(X):
+    """
+    Standardizes tabular data using StandardScaler.
+
+    Arguments:
+        X (numpy.ndarray): Tabular data of shape (n_samples, n_features)
+
+    Returns:
+        numpy.ndarray: The standardized version of the input data,
+                       with the same shape as X.
+    """
+    scaler = preprocessing.StandardScaler()
+    return scaler.fit_transform(X)
